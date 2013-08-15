ca63adbf0da1056715a278b6b657c043ebe9ef3f
tim geaney <timgeaney24@gmail.com>
2013-08-15 19:39:47 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeetupClone::Application.config.secret_token = '301a78911f6beefdf10fec31e5f69cbaa90edbb116be45e2c046484e439c7896bee0787eb552fa85b43fd86e453310a97ce835b5efe175743780a6ef8a6a8eef'
